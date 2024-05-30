27244a082aa5ca2f6a2505d9814c6a93488d3a22
Hung12423 <hungnvhe161335@fpt.edu.vn>
2024-05-30 19:12:30 +0700

    new

--- Group2_SE1804_SWP291_ClothesShop/src/java/context/OrderDAO.java
+++ Group2_SE1804_SWP291_ClothesShop/src/java/context/OrderDAO.java
@@ -18,10 +18,14 @@ import java.util.logging.Logger;
 
 public class OrderDAO extends DBContext {
 
-    public void createOrder(Order order) {
+    public void addOrder(Order order) {
         try {
+<<<<<<< Updated upstream
             connection.setAutoCommit(false);
             String sql = "INSERT INTO Orders (ordeDate, totalPrice, address, status, cartid) VALUES (?, ?, ?, ?, ?)";
+=======
+            String sql = "INSERT INTO orders (orderDate, totalPrice, address, status, cartid) VALUES (?, ?, ?, ?, ?)";
+>>>>>>> Stashed changes
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setString(1, order.getOrderDate());
             stm.setInt(2, order.getTotalPrice());
@@ -29,40 +33,12 @@ public class OrderDAO extends DBContext {
             stm.setString(4, order.getStatus());
             stm.setInt(5, order.getCartid());
             stm.executeUpdate();
-
-            String sql_get_orderid = "SELECT @@IDENTITY AS oid";
-            PreparedStatement stm_get_orderid = connection.prepareStatement(sql_get_orderid);
-            ResultSet rs = stm_get_orderid.executeQuery();
-            if (rs.next()) {
-                order.setOid(rs.getInt("oid"));
-            }
-            connection.commit();
-        } catch (SQLException ex) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex1) {
-                Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, null, ex1);
-            }
-            Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                connection.setAutoCommit(true);
-            } catch (SQLException ex) {
-                Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
-    
-    
-    public ArrayList<Order> listOrder() {
-        ArrayList<Order> orders = new ArrayList<>();
-        try {
-            String sql = "SELECT oid, orderDate, totalPrice, address, status, cartid FROM orders";
-            PreparedStatement stm = connection.prepareStatement(sql);
-            ResultSet rs = stm.executeQuery();
-            while (rs.next()) {
-                Order order = new Order();
 
+<<<<<<< Updated upstream
                 order.setOid(rs.getInt("oid"));
                 order.setOrderDate(rs.getString("orderDate"));
                 order.setTotalPrice(rs.getInt("totalPrice"));
@@ -82,35 +58,25 @@ public class OrderDAO extends DBContext {
 
     
     public void UpdateOrder(Order order) {
+=======
+    // Sửa thông tin đơn hàng
+    public void updateOrder(Order order) {
+>>>>>>> Stashed changes
         try {
-            connection.setAutoCommit(false);
-            String sql = "UPDATE orders \n"
-                    + "SET orderDate = ?, totalPrice = ?, address = ?, status = ?, cartid = ?\n"
-                    + "WHERE oid = ?";
+            String sql = "UPDATE orders SET orderDate = ?, totalPrice = ?, address = ?, status = ?, cartid = ? WHERE oid = ?";
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setString(1, order.getOrderDate());
             stm.setInt(2, order.getTotalPrice());
             stm.setString(3, order.getAddress());
-            stm.setString(4, order.getStatus());
+            stm.setInt(4, order.getStatus());
             stm.setInt(5, order.getCartid());
             stm.setInt(6, order.getOid());
             stm.executeUpdate();
-            connection.commit();
-        } catch (SQLException ex) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex1) {
-                Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, null, ex1);
-            }
-            Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                connection.setAutoCommit(true);
-            } catch (SQLException ex) {
-                Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
+<<<<<<< Updated upstream
     
     public ArrayList<Order> pagging(int pageindex, int pagesize) {
         ArrayList<Order> orders = new ArrayList<>();
@@ -278,6 +244,37 @@ public class OrderDAO extends DBContext {
                 order.setAddress(resultSet.getString("address"));
                 order.setStatus(resultSet.getString("status"));
                 order.setCartid(resultSet.getInt("cartid"));
+=======
+
+    // Xóa đơn hàng
+    public void deleteOrder(int oid) {
+        try {
+            String sql = "DELETE FROM orders WHERE oid = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, oid);
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Lấy danh sách các đơn hàng phân trang
+    public ArrayList<Order> getOrdersPaging(int index, int pageSize) {
+        ArrayList<Order> orders = new ArrayList<>();
+        try {
+            String sql = """
+                        SELECT * FROM orders
+                        ORDER BY oid
+                        OFFSET ? ROWS
+                        FETCH NEXT ? ROWS ONLY;
+                        """;
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, (index - 1) * pageSize);
+            stm.setInt(2, pageSize);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Order order = extractOrder(rs);
+>>>>>>> Stashed changes
                 orders.add(order);
             }
         } catch (SQLException e) {
@@ -285,6 +282,7 @@ public class OrderDAO extends DBContext {
         }
         return orders;
     }
+<<<<<<< Updated upstream
     
  public Order findOrderByOid(int oid) {
     Order order = null;
@@ -308,6 +306,52 @@ public class OrderDAO extends DBContext {
         Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, null, ex);
     }
     return order;
+=======
+
+    // Đếm số lượng đơn hàng
+    public int countOrders() {
+        try {
+            String sql = "SELECT COUNT(*) FROM orders";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    // Trích xuất thông tin đơn hàng từ ResultSet
+    private Order extractOrder(ResultSet rs) throws SQLException {
+        int oid = rs.getInt("oid");
+        String orderDate = rs.getString("orderDate");
+        int totalPrice = rs.getInt("totalPrice");
+        String address = rs.getString("address");
+        int status = rs.getInt("status");
+        int cartid = rs.getInt("cartid");
+        return new Order(oid, orderDate, totalPrice, address, status, cartid);
+    }
+    
+    public Order getOrderById(int oid) {
+        Order order = null;
+        try {
+            String sql = "SELECT * FROM orders WHERE oid = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, oid);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                order = extractOrder(rs);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return order;
+    }
+    
+   
+>>>>>>> Stashed changes
 }
 
  public void deleteOrder(Order order) {
--- Group2_SE1804_SWP291_ClothesShop/src/java/controller/Order/createController.java
+++ Group2_SE1804_SWP291_ClothesShop/src/java/controller/Order/createController.java
@@ -1,33 +1,66 @@
-/*
+*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+package controller.Category;
 
-package controller.Order;
-
-
-import context.OrderDAO;
-import model.Order;
-
+import context.CategoryDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import context.CategoryDAO;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "CreateOrderServlet", urlPatterns = {"/orders/create"})
-public class createController extends HttpServlet {
+/**
+ *
+ * @author chien
+ */
+public class addCate extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/order/create.jsp").forward(req, resp);
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet addCate</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet addCate at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+<<<<<<< Updated upstream
         String orderDate = req.getParameter("orderDate");
         int totalPrice = Integer.parseInt(req.getParameter("totalPrice"));
         String address = req.getParameter("address");
@@ -45,10 +78,35 @@ public class createController extends HttpServlet {
         orderDAO.createOrder(order);
 
         resp.sendRedirect(req.getContextPath() + "/orders");
+=======
+       
+        
+        request.getRequestDispatcher("listCate").forward(request, response);
+>>>>>>> Stashed changes
     }
 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
 }
